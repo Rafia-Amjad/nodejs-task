295281243c299659ec591ae1186b47b2a35eb408
Rafia-Amjad <163378796+Rafia-Amjad@users.noreply.github.com>
2024-05-13 15:57:17 +0500

    Add files via upload

---
+++ new2.js
@@ -0,0 +1,2 @@
+console.log("Rafia Amjad")
+//console.log(window); //this line will not execute on node js
---
+++ varfile.js
@@ -0,0 +1,11 @@
+var name = "Rafia Amjad"
+let age = 10;
+const salary = 100;
+//let age = 61; we can't use it again
+//salary = 10; we cannot use it again and we cannot modify it also
+//var name = "Muhammad"; //we can use it again and also we can modify it
+
+var num1 = 100;
+var num2 = 2.5;
+
+console.log(name + num1 + num2);
